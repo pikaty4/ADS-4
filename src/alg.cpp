fc223a30338f7ae62a74ac452bfc33af6e0714fb
pikaty4 <71702322+pikaty4@users.noreply.github.com>
2022-06-05 15:46:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,70 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-  return 0;
+int binarySearch(int arr[], int a, int b, int number) {
+    if (a <= b) {
+        int ser = (a + b) / 2;
+        if (arr[mid] == number) {
+            int i = 1;
+            int c = 1;
+            while (arr[ser - i] == number && ser - i >= 0)
+            { 
+              i++; 
+              c++; 
+            }
+            i = 1;
+            while (arr[ser + i] == number) { 
+              i++;
+              c++;
+            }
+            return c;
+        }
+        if (arr[ser] < number)
+            return binarySearch(arr, ser + 1, b, number);
+        if (arr[ser] > number)
+            return binarySearch(arr, a, ser - 1, number);
+        
+    }
+    return 0;
 }
-int countPairs2(int *arr, int len, int value) {
-  return 0;
+
+int countPairs1(int* arr, int val, int len) {
+    int c = 0;
+    for (int i = 0; i < len - 1; i++) {
+        for (int k = i + 1; k < len; k++) {
+            if (*(arr + i) + *(arr + k) == val) {
+                c++;
+            }
+        }
+    }
+    return c;
 }
-int countPairs3(int *arr, int len, int value) {
-  return 0;
+
+int countPairs2(int* arr, int val, int len) {
+    int c = 0;
+    int i = 0;
+    int d = 0;
+    int f = 0;
+    while (i < len) {
+        d = 1;
+        while (d < len - 1) {
+            s = *(arr + i) + *(arr + i + d);
+            if (f > val) break;
+            if (f == val) c++;
+            d++;
+        }
+        i++;
+    }
+    return c;
+}
+
+int countPairs3(int* arr, int val, int len) {
+    int i = 0;
+    int c = 0;
+    int s, s2;
+    while (i < len) {
+        if (*(arr + i) + *(arr + i + 1) > val) return c;
+        s = binarySearch(arr + i + 1, 0, len - i - 2, val - *(arr+i));
+        c += s;
+        i++;
+    }
+    return c;
 }
